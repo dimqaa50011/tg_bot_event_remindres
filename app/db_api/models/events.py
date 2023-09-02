1c9dd1dffee28e15e5769ea6827263e2cfb3c026
Dmitry <dz5011@yandex.ru>
2023-09-02 16:33:44 +0300

    update constraint: unique tg_id in user model and unique title in event model

--- app/db_api/models/events.py
+++ app/db_api/models/events.py
@@ -6,6 +6,6 @@ from .base_model import BaseModel
 class EventModel(BaseModel):
     __tablename__ = "event"
 
-    title = Column(String(128), nullable=False)
+    title = Column(String(128), nullable=False, unique=True)
     description = Column(String(512), nullable=True)
     date_event = Column(DateTime, nullable=False)
--- app/db_api/models/users.py
+++ app/db_api/models/users.py
@@ -5,9 +5,9 @@ from .base_model import BaseModel
 
 class UserModel(BaseModel):
     __tablename__ = "user"
-    
-    tg_id = Column(BigInteger, nullable=False, index=True)
+
+    tg_id = Column(BigInteger, nullable=False, index=True, unique=True)
     first_name = Column(String(length=64), nullable=True)
     last_name = Column(String(length=64), nullable=True)
-    username = Column(String(length=64), nullable=True)
+    username = Column(String(length=64), nullable=True, unique=True)
     joined_date = Column(DateTime)
